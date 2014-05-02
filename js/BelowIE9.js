dbc87dffd9523e2e2d3ba7049d47fb1d9efab2f1
BlueFisher <blue_fisher@qq.com>
2014-05-02 13:35:58 +0800

    1.0
    Only contain .js

--- js/BelowIE9.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-	document.write("<h1 style=\"font-size:75px; color:red\">OH MY GOD 你居然还在用IE9以下的版本</h1>")
-	document.write("<h1 style=\"font-size:75px; color:red\">快去下载IE10 IE11 Firefox 或 Chrome 吧！</h1>")
-	document.write("<a href=\"http://www.firefox.com.cn/\" style=\"font-size:50px\">去下载Firefox </a><br/>")
-	document.write("<a href=\"https://www.google.com/intl/zh-CN/chrome/browser/\" style=\"font-size:50px\">去下载Chrome </a><br/>")
-	document.write("<a href=\"http://ie.microsoft.com/\" style=\"font-size:50px\">去下载最新的IE </a>")
-}
